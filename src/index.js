730169e28ca8dd44c1bed1c124bd60c679a2e9ff
elnurmagerramli23 <elnurmagerramli@gmail.com>
2020-03-02 18:56:08 +0400

    latest

--- src/index.js
+++ src/index.js
@@ -2,48 +2,124 @@ let ArrayList = {
     array: [],
     size: 0,
     init: function(argument) {
-        if((typeof argument) !== 'object' || argument === null) {
-            return 'Please input an array'
-        } else {
+        if (!argument) return 'Please input an array';
         ArrayList.array = argument;
         ArrayList.size = argument.length;
-        }
+
+        return ArrayList.array
     },
     addAtEnd: function(arrElm) {
-        if((typeof arrElm) !== 'object' || arrElm === null) {
-            return 'Please input an array'
-        } else {
-        const arr = ArrayList.array;
-        arr[arr.length] = arrElm;
-        ArrayList.size = arr.length;
-        }
+        if (!arrElm)  return 'Please input an array';
+            const arr = ArrayList.array;
+            arr[arr.length] = arrElm;
+            ArrayList.size = arr.length;
+
+            return arr;
     },
     clearObject: function() {
         ArrayList.array = [];
-        ArrayList.size = 0
+        ArrayList.size = 0;
+
+        return ArrayList.array
     },
     clearFromEnd: function() {
         const arr = ArrayList.array;
         arr.length = [arr.length - 1];
         ArrayList.size = arr.length;
+        
+        return ArrayList.array;
     },
-    clearFirst: function() {
-        let arr = ArrayList.array;   
-        let b = [];     
+    deleteFirst: function() {
+        const arr = ArrayList.array;   
+        let newArray = [];     
+
         for (let i = 1; i < arr.length; i++) {
-            b[i] = arr[i];
-        }
-        arr = b;
+            newArray[i-1] = arr[i];
+        };
+        ArrayList.array = newArray;
+        ArrayList.size -= 1;
+
+        return arr[0];
     },
-    
+    addToFirst: function(argument) {
+        if (!argument) return 'Please check your input';
+        const arr = ArrayList.array;
+        let newArr = [argument];
+
+        for (let i = 0; i < arr.length; i++) newArr[i + 1] = arr[i]
+        ArrayList.array = newArr;
+        ArrayList.size += 1;
+
+        return ArrayList.array;
+    },
+    revArr: function() {
+        const arr = ArrayList.array;
+        let secArr = [];
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            secArr[secArr.length] = arr[i];
+        }        
+        ArrayList.array = secArr;
+        ArrayList.size = secArr.length;
+
+        return ArrayList.array
+    },
+    checkForInclude: function(argument) {
+        if (!argument) return 'Please input an array';
+        const arr = ArrayList.array;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (argument === arr[i]) {
+                return "true"
+            }  
+        };
+
+        return "false"
+    },
+    checkForIndex: function(argument) {
+        if (!argument) return 'Please input an array';
+        const arr = ArrayList.array;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (argument === arr[i]) return i
+        }
+        ArrayList.size = arr.length;
+
+        return '-1';
+    },
+    changeToString: function() {
+        const arr = ArrayList.array;
+        
+        return '' + arr;
+    },
+    getSliceAction: function(start, end) {
+        if((typeof start) !== 'number' || (typeof end) !== 'number') {
+        return 'Please input a number'
+        }
+        const arr = ArrayList.array;
+        let newArr = [];
+        let counter = 0;
+
+        if (end) {        
+            for (let i = start; i < end; i++) {
+                newArr[counter++] = arr[i];
+            }
+        } else {
+            for (let i = start; i < arr.length; i++) {
+                newArr[counter++] = arr[i];
+            }
+        }
+        ArrayList.array = newArr;
+        ArrayList.size = newArr.length;
+
+        return newArr
+    },
 };
+ 
 
 
-// addToFirst: function(arrElm) {
-//     let arr = ArrayList.array;
-//     let b = [];
-//     for (let i = 1; i < arr.length; i++) {
-//         b[i-1] = arr[i];
-//     }
-//     arr = b;
-// },
+
+
+
+
+
