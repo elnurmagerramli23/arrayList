1f035fd68587d9da60892f55a7aea9f9e2bb233b
elnurmagerramli23 <elnurmagerramli@gmail.com>
2020-03-03 13:26:58 +0400

    new file

---
+++ src/test.js
@@ -0,0 +1,28 @@
+// Переделать ArrayList протативном стиле. Прочитай про Linked list. Свойства будут :
+function ArrayList(array) {
+    this.array = array;
+    this.size = array.length;
+}
+
+ArrayList.prototype.clear = function () {
+    this.array = [];
+    this.size = 0;
+};
+
+function Node(value) {
+    this.value = value;
+    this.next = null;
+}
+
+let node2 = new Node(5);
+console.log(node2);
+
+
+function LinkedList() {
+    this.root = null;
+    this.size = 0;
+};
+
+LinkedList.prototype.addElm = function(value) {
+    const tempNode = new Node(value)
+};
